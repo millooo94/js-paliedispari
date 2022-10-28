3963a6a97584c4f0815feafcec90f7286f7f2680
millooo94 <camillomusmeciunict@gmail.com>
2022-10-28 15:08:29 +0200

    Add palindrome

---
+++ palindroma/js/script.js
@@ -0,0 +1,19 @@
+const userWord = prompt('Dammi una parola');
+
+
+if (isPalindrome(userWord)) {
+    alert('palindrome');
+} else {
+    alert('not palindrome');
+    
+}
+
+
+function isPalindrome(word) {
+
+    if (Array.from(word).toString() == (Array.from(word).reverse()).toString()) {
+        return true 
+    } else {
+        return false
+    }
+}
